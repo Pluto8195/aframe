7bf1945fa53698ed8c2bcd2c9eade397269a01b8
Michael Saunders <michael@Michaels-Air.fios-router.home>
2018-11-20 21:42:36 -0500

    updated dist

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.8.2 (Date 2018-11-18, Commit #7f9c5bf)');
+console.log('A-Frame Version: 0.8.2 (Date 2018-11-21, Commit #84521d05)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
